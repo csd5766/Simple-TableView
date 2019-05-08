fa2a51a550599edda301599289d3785599498742
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-05-08 14:46:22 +0900

    Initial Commit

---
+++ Simple TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple TableView
+//
+//  Created by D7702_09 on 2019. 5. 8..
+//  Copyright © 2019년 csd5766. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
